81cd565916271d3990d06ec50fb3f88090c79f8a
Jürgen <behighspy@yahoo.de>
2018-06-07 15:32:07 +0200

    oon click event  for dice one

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,51 @@ function getRandomInt(min, max) {
 
 let diceOne = {
 	value: 1,
-	roll : function(value){
-		this.value = value;
-		return value;
+	roll : function(){
+		this.value = getRandomInt(1, 6);
+		return this.value;
 	}
 };
+let diceTwo = {
+	value: 1,
+	roll : function(){
+		this.value = getRandomInt(1, 6);
+		return this.value;
+	}
+};
+// console.log(diceOne.value);
+//
+// diceOne.roll();
+// console.log(diceOne.value);
+//
+// diceOne.roll();
+// console.log(diceOne.value);
+//
+// diceOne.roll();
+// console.log(diceOne.value);
+
+
+
+let button = document.body.querySelector("#roll-dice");
+
+
+let rollDice = button.addEventListener("click", event =>{
+event.preventDefault;
+
+let containerOne = document.body.querySelector("#container-1");
+const diceUno = diceOne.roll();
+let diceOneOutput = document.createTextNode(diceUno);
+containerOne.appendChild(diceOneOutput);
+
+
+
+
+// let containerOne = document.body.querySelector("container-1");
+// let text = document.createTextNode("haha, just kidding");
+// containerOne.appendChild(text);
+
+diceTwo.roll();
 console.log(diceOne.value);
+console.log(diceTwo.value);
 
-diceOne.roll(getRandomInt(1, 7));
-
-console.log(getRandomInt(1,7));
-
-
-let containerOne = document.body.querySelector("container-1");
+});
